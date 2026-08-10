c67c579237b7c12d45e45e819a126a038ed41a13
Daniel Cyubahiro <cyubahirodaniel@gmail.com>
2026-08-10 09:20:33 +0200

    chore: move CreateProduct command and handler for product creation

--- src/product/application/use-cases/commands/create-product/create-product.handler.ts
+++ src/product/application/use-cases/commands/create-product/create-product.handler.ts
@@ -1,8 +1,8 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CreateProductCommand } from './create-product.command';
 import { Inject } from '@nestjs/common';
-import type { ProductRepository } from '../../ports/product.repository';
-import { Product } from '../../../domain/entities/product.entity';
+import type { ProductRepository } from '../../../ports/product.repository';
+import { Product } from '../../../../domain/entities/product.entity';
 
 @CommandHandler(CreateProductCommand)
 export class CreateProductHandler implements ICommandHandler<CreateProductCommand> {

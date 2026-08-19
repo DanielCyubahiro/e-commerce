2cce9b432bdc006988b688d9f8268d4500af1cf9
Daniel Cyubahiro <cyubahirodaniel@gmail.com>
2026-08-19 11:52:18 +0200

    feat(identity): add credentials, refresh_tokens and one_time_tokens tables

--- src/shared/infrastructure/database/postgres/schema/index.ts
+++ src/shared/infrastructure/database/postgres/schema/index.ts
@@ -1,2 +1,5 @@
+export { credentials } from './credentials.schema';
+export { oneTimeTokens, tokenPurpose } from './one-time-tokens.schema';
 export { products } from './products.schema';
+export { refreshTokens } from './refresh-tokens.schema';
 export { userRole, users } from './users.schema';
